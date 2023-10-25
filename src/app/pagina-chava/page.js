e5ebaf72ea0ee1c6917330ffd5ce99d7c0731238
Salvador Chavez Acosta <salvador.chavez.acosta.18@gmail.com>
2023-10-25 11:37:21 -0600

    pagina-chava commit

---
+++ src/app/pagina-chava/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Hola como estas</h1>
+        <p>Estoy cansado gfe ya jubileme alv xd</p>
+    </div>
+  )
+}
+
+export default page
